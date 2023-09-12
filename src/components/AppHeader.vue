3f135158cc4dda91f39bbae51d9de3b59506b477
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2023-09-12 15:23:34 +0200

    add setup + scss & Components

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<template>
+  <header>
+    <div id="logo">logo</div>
+    <h1>Yu-Gi-Oh Api</h1>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+  background-color: #fff;
+  height: 100px;
+}
+</style>
---
+++ src/components/ListCard.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: "ListCard",
+};
+</script>
+
+<template>
+  <header>
+    <div class="container"></div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  background-color: #fff;
+  width: 70%;
+  margin: 5px auto;
+}
+// DEBUG
+.container {
+  min-height: 500px;
+}
+</style>
