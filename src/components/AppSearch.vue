145268c13970c4fb2669ade786b985ee16b1da98
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2023-09-13 18:24:30 +0200

    Update $emit form Filter

--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -13,7 +13,12 @@ export default {
 <template>
   <section>
     <form>
-      <select name="archetype" id="searchType" v-model.trim="store.searchType">
+      <select
+        name="archetype"
+        id="searchType"
+        v-model.trim="store.searchType"
+        @change="$emit('searchType')"
+      >
         <option
           v-for="archetype in store.arrayOfType"
           :value="archetype.archetype_name"
@@ -22,12 +27,13 @@ export default {
         </option>
       </select>
 
-      <button type="submit" @click.prevent="$emit('searchType')">cerca</button>
+      <!-- <button type="submit" @click.prevent="$emit('searchType')">cerca</button> -->
     </form>
   </section>
 </template>
 
 <style lang="scss" scoped>
+@use "./../styles/partials/variables.scss" as *;
 #searchType {
   margin: 5px 0;
   width: 100px;
@@ -35,5 +41,13 @@ export default {
   background-color: #fff;
   border: 1px solid #d4d3cf;
   border-radius: 5px;
+}
+button {
+  cursor: pointer;
+  padding: 10px;
+  border: 1px solid #fff;
+  border-radius: 10px;
+  color: $primaryColor;
+  background-color: #fff;
 }
 </style>
